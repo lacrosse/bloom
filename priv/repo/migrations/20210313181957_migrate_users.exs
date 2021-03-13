2d7688b4f25413f321569e74a15e02381b33a8ca
Alexander Skiba <alex@aeriform.one>
2021-03-14 02:11:38 +0700

    Remove Bloom.External.LastFM.User agent

--- priv/repo/migrations/20210313181957_migrate_users.exs
+++ priv/repo/migrations/20210313181957_migrate_users.exs
@@ -1,3 +1,29 @@
+defmodule Bloom.External.LastFM.User do
+  @db "lastfm_users.txt"
+
+  def start_link do
+    Agent.start_link(fn -> get_users() end, name: __MODULE__)
+  end
+
+  def table_for_db() do
+    Agent.get(__MODULE__, & &1)
+  end
+
+  defp get_users() do
+    with {:ok, db} <- File.read(@db) do
+      db
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [id, username] = String.split(line, " ")
+        {String.to_integer(id), username}
+      end)
+      |> Enum.into(%{})
+    else
+      {:error, _} -> %{}
+    end
+  end
+end
+
 defmodule Bloom.Repo.Migrations.MigrateUsers do
   use Ecto.Migration
   alias Bloom.User
