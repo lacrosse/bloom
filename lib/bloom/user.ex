5b329f13b4975808b318f9c557ed81c3a9c05700
Alexander Skiba <alex@aeriform.one>
2021-03-14 04:20:58 +0700

    Change primary key in users

--- lib/bloom/user.ex
+++ lib/bloom/user.ex
@@ -2,15 +2,15 @@ defmodule Bloom.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @primary_key {:telegram_id, :integer, autogenerate: false}
+  @primary_key {:id, :integer, autogenerate: false}
   schema "users" do
     field(:lastfm_username, :string)
   end
 
   def changeset(user, params \\ %{}) do
     user
-    |> cast(params, [:telegram_id, :lastfm_username])
-    |> validate_required([:telegram_id])
-    |> unique_constraint(:telegram_id)
+    |> cast(params, [:id, :lastfm_username])
+    |> validate_required([:id])
+    |> unique_constraint(:id)
   end
 end
